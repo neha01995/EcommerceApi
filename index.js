1eddc1e3574d9bcb3fc46dd0a591b45d5289da32
Neha Singla <nehasingla7180@gmail.com>
2020-05-17 10:34:15 +0530

    created config mongoose.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
-
+const bodyParser = require('body-parser');
 const port = 8000;
+// create express app
 const app = express();
 
+// parse requests of content type
+app.use(bodyParser.urlencoded({extended: true}));
+// parse request of content-type
+app.use(bodyParser.json());
 
 
+// Defining a simple route
 app.get('/', (req, res) => {
     res.json({"message": "welcome guys" });
 });
 
+
+// Listen for requests
 app.listen(port, function(err){
     if(err){console.log(`Error in running the server, ${err}`)};
     console.log(`Server is running on port: ${port}`);
