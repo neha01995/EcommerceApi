1baf3030269e911751c61a10ce6d957390c82312
Neha Singla <nehasingla7180@gmail.com>
2020-05-17 11:05:33 +0530

    just deifned the name of controller now next we will write controller one by one

--- controller/productController.js
+++ controller/productController.js
@@ -2,6 +2,26 @@ const mongoose = require('mongoose');
 
 const ProductSchema = require('../models/ProductSchema');
 
-module.exports.home = function(req, res){
-    res.json({"message": "welcome guys" });
+// module.exports.home = function(req, res){
+//     res.json({"message": "welcome guys" });
+// }
+
+// for add the product
+module.exports.productAdd = function(req, res){
+
+}
+
+// for fetching the product
+module.exports.fetchProduct = function(req, res){
+
+}
+
+// For updating the product
+module.exports.productUpdate = function(req, res){
+
+}
+
+// For deleting a product
+module.exports.productDelete = function(req, res){
+    
 }
